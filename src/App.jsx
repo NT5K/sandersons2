25ac515f6962d7c7c87429e94e87f56976670ec8
NT5K <nicholastysh@gmail.com>
2025-07-23 21:40:15 -0400

    add back main.css

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Cocktails from "./pages/Cocktails";
 import Home from "./pages/Home";
 import Tickets from "./pages/Tickets";
 import "./App.css"; 
+import "./styles/main.css";
 import Contact from "./pages/Contact";
 
 function App() {
